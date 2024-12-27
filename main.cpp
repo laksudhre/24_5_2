c1be99a2bf953a6294452700a01a90cc89dc5470
WAIT <laksudhre@gmail.com>
2024-12-27 08:32:28 +0300

    вжух

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//
-// Created by vvv on 7/26/24.
-//
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,27 +5,24 @@
 #include <iomanip>
 
 struct Person {
+    Person(const std::string& name, const std::time_t birthday) : name(name), birthday(birthday) {}
+    Person() : name(), birthday() {}
     std::string name;
     std::time_t birthday{};
 };
 
 Person getNextPerson(const std::vector<Person> &persons) {
     const auto t = std::time(nullptr);
-    const auto *current = std::gmtime(&t);
-    const auto currentMonth = current->tm_mon;
-    const auto currentDay = current->tm_mday;
     Person nextPerson;
     for (const auto &[name, birthday] : persons) {
-        const auto *tm = std::gmtime(&birthday);
-        const auto birthMonth = tm->tm_mon;
-        const auto birthDay = tm->tm_mday;
-        if (birthMonth > currentMonth || (birthMonth == currentMonth && birthDay > currentDay)) {
-            const auto buf = std::gmtime(&nextPerson.birthday);
-            const auto bufMonth =  buf->tm_mon;
-            const auto bufDay = buf->tm_mday;
-            if (!nextPerson.name.empty() && birthMonth > bufMonth || (birthMonth == bufMonth && birthDay > bufDay)) {
-                nextPerson.name = name;
+        if (nextPerson.name.empty()) {
+            nextPerson.birthday = birthday;
+            nextPerson.name = name;
+        }
+        if (birthday > t) {
+            if (nextPerson.birthday > birthday) {
                 nextPerson.birthday = birthday;
+                nextPerson.name = name;
             }
         }
     }
@@ -47,13 +40,13 @@ int main() {
 
         std::cout << "Enter the date of birth (YYYY/MM/DD): ";
         const auto t = std::time(nullptr);
-        const auto in = std::gmtime(&t);
+        const auto in = std::localtime(&t);
         std::cin >> std::get_time(in, "%Y/%m/%d");
         newPerson.birthday = std::mktime(in);
         persons.push_back(newPerson);
     }
-    const auto nextPerson = getNextPerson(persons);
-    std::cout << "Name: " << nextPerson.name << std::endl;
-    std::cout << "Birthday: " << std::put_time(std::gmtime(&nextPerson.birthday), "%m/%d") << std::endl;
+    const auto [name, birthday] = getNextPerson(persons);
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Birthday: " << std::put_time(std::localtime(&birthday), "%Y/%m/%d") << std::endl;
     return 0;
 }
